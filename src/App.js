701813d063d3355e2fe71c0d6d6a1f15098d4f1f
boy <boydoubleb@gmail.com>
2020-05-18 14:43:59 +0700

    [ADD] basic-react :rocket:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import UserOutput from "./User/UserOutput";
+import UserInput from "./User/UserInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userName: "Warakorn",
+  };
+
+  changeNameHandler = (event) => {
+    this.setState({
+      userName: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div class="App">
+        <UserOutput userName={this.state.userName}></UserOutput>
+        <UserOutput userName={this.state.userName}></UserOutput>
+        <UserInput
+          changed={this.changeNameHandler}
+          userName={this.state.userName}
+        ></UserInput>
+      </div>
+    );
+  }
 }
 
 export default App;
